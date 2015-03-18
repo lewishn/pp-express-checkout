5b8a18278837326587305d2e61c645a919e3fd79
Lewis <lewis.haris@gmail.com>
2015-03-18 21:21:39 +0800

    Remove psql connection test

--- psql_connect.php
+++ psql_connect.php
@@ -7,13 +7,4 @@
 	
 	
 	$pg_conn = pg_connect(pg_connection_string_from_database_url());
-	
-	$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-	print "<pre>\n";
-	if (!pg_num_rows($result)) {
-  		print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-	} else {
-  		print "Tables in your database:\n";
-  		while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-	}
 ?>
